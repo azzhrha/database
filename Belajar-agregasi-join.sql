478fa8615ec1d3a8386fb64d57acc62e8c1a3c6e
Luthfiyah Azzahra H <110676933+azzhrha@users.noreply.github.com>
2022-08-22 11:54:10 +0700

    Add files via upload

---
+++ Belajar-agregasi-join.sql
@@ -0,0 +1,48 @@
+SELECT nama, kelas, MIN(nilai)FROM
+siswa;
+
+SELECT tempatlahir, MIN(nilai)
+FROM siswa GROUP BY tempatlahir;
+
+SELECT tempatlahir, MIN(nilai), MAX(nilai)
+FROM siswa GROUP BY tempatlahir;
+
+SELECT tempatlahir, MIN(nilai) AS nilai_terendah,
+MAX(nilai) AS nilai_tertinggi
+FROM siswa GROUP BY tempatlahir;
+
+SELECT tempatlahir, COUNT(tempatlahir) AS orang_lahir
+FROM siswa GROUP BY tempatlahir;
+
+SELECT jk, SUM(nilai) AS jumlah_nilai
+FROM siswa GROUP BY jk;
+
+SELECT jk, AVG(nilai) AS rata_rata
+FROM siswa GROUP BY jk;
+
+SELECT siswa.* , pinjambuku.*
+FROM siswa 
+JOIN pinjambuku 
+ON siswa.nis = pinjambuku.nis;
+
+SELECT siswa.* , pinjambuku.*
+FROM siswa 
+RIGHT JOIN pinjambuku
+ON siswa.nis = pinjambuku.nis;
+
+SELECT siswa.* , pinjambuku.*
+FROM siswa 
+LEFT JOIN pinjambuku
+ON siswa.nis = pinjambuku.nis;
+
+SELECT siswa.* , pinjambuku.*
+FROM siswa 
+LEFT JOIN pinjambuku
+ON siswa.nis = pinjambuku.nis
+WHERE pinjambuku.nis IS NULL;
+
+SELECT siswa.* , pinjambuku.*
+FROM siswa 
+RIGHT JOIN pinjambuku
+ON siswa.nis = pinjambuku.nis
+WHERE siswa.nis IS NULL;
